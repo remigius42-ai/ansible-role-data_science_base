234e259185884f572319bd495e47a74450bf43d9
Andreas Schmidt <andreas.remigius@gmail.com>
2017-11-28 09:18:25 +0100

    Factor out jupyter tasks with extensions
    - Create jupyter.yml and import it in main.yml
    - Extend jupyter.yml tasks with
      - allowing external hosts
      - save hook
      - nbextensions configuration
      - nbdime activation

---
+++ files/jupyter_save_hooks.py
@@ -0,0 +1,34 @@
+def scrub_output_and_execution_count(model, **kwargs):
+    """Scrub output and execution count before saving notebooks.
+
+    Scrubbing may be disabled for an entrire notebook by setting
+    the notebook metadata
+    {
+      'pre_save_hook_config': {
+        'is_output_preserved': true
+      }
+    }
+    """
+    # check if scrubbing is overridden
+    try:
+        notebook_metadata = model['content']['metadata']
+        if notebook_metadata['pre_save_hook_config']['is_output_preserved']:
+            return
+    except KeyError:
+        pass
+    # only run on notebooks
+    if model['type'] != 'notebook':
+        return
+    # only run on nbformat v4
+    if model['content']['nbformat'] != 4:
+        return
+
+    for cell in model['content']['cells']:
+        if cell['cell_type'] != 'code':
+            continue
+        cell['outputs'] = []
+        cell['execution_count'] = None
+
+
+def pre_save_hook(model, **kwargs):
+    scrub_output_and_execution_count(model, **kwargs)
